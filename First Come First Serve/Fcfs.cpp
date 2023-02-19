2f15e64b52c9a218f78aaeb0013f1a131b87b43b
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-02-19 23:24:04 +0600

    Update Fcfs.cpp

--- First Come First Serve/Fcfs.cpp
+++ First Come First Serve/Fcfs.cpp
@@ -1,66 +1,126 @@
 #include<bits/stdc++.h>
 using namespace std;
+int turn_time[100],burst_time[100],sum=0;
+vector<int>gant_value;
+void gantchart(string str){
+	int i,j,cnt=1;
+	string res = "|";
+    cout << "Gant chart : "<<endl;
+    gant_value.push_back(0);
+    for(i=0;i<str.size()-1;i++){
+        if(str[i]==str[i+1])
+            cnt++;
+        else{
+            gant_value.push_back(gant_value[gant_value.size()-1]+cnt);
+            for(j=0;j<cnt/2;j++)
+                res+=' ';
+            res+='p';
+            res+=str[i];
+            for(j=0;j<cnt/2;j++)
+                res+=' ';
+            res+='|';
+            cnt = 1;
+        }
+    }
+    gant_value.push_back(gant_value[gant_value.size()-1]+cnt);
+    for(j=0;j<cnt/2;j++)
+        res+=' ';
+    res+='p';
+    res+=str[i];
+    for(j=0;j<cnt/2;j++)
+        res+=' ';
+    res+='|';
+    
+    for(i=0;i<res.size();i++)
+        cout <<'-';
+    cout << endl;
+    cout << res << endl;
+    for(i=0;i<res.size();i++)
+        cout <<'-';
+    cout << endl;
+    
+    int in = 1;
+    cout <<0;
+    for(i=1;i<res.size()-1;i++){
+        if(res[i+1]=='|'){
+            printf("%2d",gant_value[in]);
+            in++;
+        }
+        else if(res[i]!='|')
+            cout <<' ';
+    }
+}
+
+
+void response_time(int n,string str){
+	int res_time[n],i;
+	for(i=0;i<n;i++)
+		res_time[i] = -1;
+	
+	for(i=0;i<str.size();i++){
+		int temp;
+		temp = str[i]-'1';
+		if(res_time[temp] == -1)
+			res_time[temp] = i;
+	}
+	cout <<"\n\nRespose time : \n";
+	for(i=0;i<n;i++)
+		printf("p[%d] = %2d\n",i+1,res_time[i]);
+	cout << endl;
+}
+
+void turnaround_time(int n,string str){
+	int i;
+	for(i=0;i<n;i++){
+		turn_time[i] = -1;
+	}
+	for(i=str.size()-1;i>=0;i--){
+		int temp;
+		temp = str[i]-'1';
+		if(turn_time[temp]==-1){
+			turn_time[temp] = i+1;
+		}
+	}
+	
+	cout <<"\n\nTurn arround time : \n";
+	for(i=0;i<n;i++)
+        printf("p[%d] = %2d\n",i+1,turn_time[i]);
+    cout << endl;
+}
+
+void waiting_time(int n,string str){
+	int i,wait_time[n];
+	for(i=0;i<n;i++){
+        wait_time[i] = turn_time[i]-burst_time[i];
+        sum+=wait_time[i];
+    }
+    cout <<"\n\nWaiting time : \n";
+    for(i=0;i<n;i++)
+        printf("[%d] = %2d\n",i+1,wait_time[i]);
+    cout << endl;
+	
+	float avgwt = sum*1.0/n ;
+	cout <<"\nAvarage Waiting time : "<<setprecision(3)<<avgwt<<endl;
+}
+
+
 int main(){
 	freopen("fcfs.txt", "r", stdin);
     freopen("fcfsout.txt", "w", stdout);
-    int n,i,j;
-    //cout <<"Enter the number of process : ";
+    int n,temp,i;
     cin >> n;
-    int a[n],waiting_time[n],mainsum=0,sum=0,avgsum=0,temp;
-    float avg;
     for(i=0;i<n;i++){
-        //cout <<"Enter the burst time for process "<<i+1<<": ";
-        cin >> a[i];
-        mainsum+=a[i];
-        if(i==0)
-            waiting_time[i] = 0;
-        else {
-            waiting_time[i] = waiting_time[i-1]+a[i-1];
-            avgsum+=waiting_time[i];
-        }      
-    }
-    
-    //Printing Gantt chart
-    for(i=0;i<mainsum*2+3*n+1;i++)
-    	cout <<'-';
-    cout << endl;
-    
-    cout<<"| ";
-    for(i=0;i<n;i++){
-        temp = a[i];
-        for(j=0;j<a[i]-1;j++)
-            cout <<' ';
-        cout << 'p'<<i+1;
-        for(j=0;j<a[i]-1;j++)
-            cout <<' ';
-		cout<<" | ";
-    }
-    
-    cout << endl;
-    for(i=0;i<mainsum*2+3*n+1;i++)
-    	cout <<'-';
-    cout << endl;
-   	cout << 0;
-   	
-	for(i=0;i<n;i++){
-		sum+=a[i];
-		cout <<' ';
-		for(j=0;j<2*a[i];j++)
-			cout <<' ';
-		printf("%2d",sum);
-	} 
-	
-	
-	cout <<"\n\nWaiting time : "<<endl;
-	for(i=0;i<n;i++){
-		printf("p[%d] = %d\n",i+1,waiting_time[i]);
-	}	
-	
-	cout <<"\n\nResponse time : "<<endl;
-	for(i=0;i<n;i++){
-		printf("p[%d] = %d\n",i+1,waiting_time[i]);
+    	cin >> burst_time[i];
 	}
-    
-    avg = avgsum*1.0/n;
-    cout << "\n\navarage wating time is : " << avg <<endl;
+    string str;
+    for(i=0;i<n;i++){
+    	temp = burst_time[i];
+    	while(temp--){
+    		str+=i+'1';
+		}
+	}
+    gantchart(str);
+	response_time(n,str);
+	turnaround_time(n,str);
+	waiting_time(n,str);
 }
