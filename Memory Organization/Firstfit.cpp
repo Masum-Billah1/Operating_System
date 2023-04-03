7348077b7609b7efd88dffca7b227c195ebd43ca
Masum Bilah <53857306+Masum-Billah1@users.noreply.github.com>
2023-04-03 10:11:43 +0600

    First_Fit

---
+++ Memory Organization/Firstfit.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int process_number,i,j,k;
+	freopen("memory.txt","r",stdin);
+	cin >> process_number;
+	int process[process_number],result[process_number];
+	
+	for(i=0;i<process_number;i++){
+		cin >> process[i];
+		result[i] = -1;
+	}
+			
+	int block_number;
+	cin >> block_number;
+	int block[block_number],unalocate[block_number];
+	
+	for(i=0;i<block_number;i++){
+		cin >> block[i];
+		unalocate[i] = block[i];
+	}
+	
+	for(i=0;i<process_number;i++)
+		cout << process[i]<<' ';
+	cout << endl;
+	
+	for(i=0;i<block_number;i++)
+		cout << block[i]<<' ';
+	cout << endl<<endl;
+	
+	for(i=0;i<block_number;i++){
+		for(j=0;j<process_number;j++){
+			for(k=0;k<process_number;k++)
+				cout << unalocate[k]<<' ';
+			cout << endl;
+			if(unalocate[j] >= process[i]){
+				result[i] = j+1;
+				unalocate[j] -= process[i];
+			}
+			break;
+		}
+	}
+	
+	for(i=0;i<process_number;i++)
+		cout << result[i]<<' ';
+	cout << endl;
+}
